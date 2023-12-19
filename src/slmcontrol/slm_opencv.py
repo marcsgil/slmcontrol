85a97fb9bf0db722bb559fd1370588b3465ce063
marcsgil <marcosgildeoliveira@gmail.com>
2023-12-19 17:09:43 -0300

    included experimental version with opencv

--- src/slmcontrol/slm_opencv.py
+++ src/slmcontrol/slm_opencv.py
@@ -1,45 +1,60 @@
 import cv2 as cv
 import screeninfo
 import numpy as np
-from slmcontrol.hologram import build_grid
+import threading
+import queue
 
 
 class SLMdisplay:
-    def __init__(self, monitor=1, window_name='slm'):
+    def __init__(self, monitor=-1, display_interval=150, window_name='slm'):
         """Initializes an object of the class SLMdisplay. EXPERIMENTAL
 
         Args:
-            monitor (int, optional): index that identifies the monitor in which the holograms will be shown. The primary monitor has index 0. Defaults to 1.
+            monitor (int, optional): index that identifies the monitor in which the holograms will be shown. The primary monitor has index 0. Defaults to -1.
+            display_interval (int, optional): Interval in miliseconds between each frame. Defaults to 150.
             window_name (str, optional): Name for the window. It is used as an identifier. Defaults to 'slm'.
         """
         self.monitor = screeninfo.get_monitors()[monitor]
         self.window_name = window_name
+        self.display_interval = display_interval
+        self.queue = queue.Queue()
 
+        self.queue.put(
+            np.zeros((self.monitor.height, self.monitor.width), dtype='uint8'))
+
+        self.is_open = True
+
+        thread = threading.Thread(target=self.run)
+        thread.start()
+
+    def run(self):
         cv.namedWindow(self.window_name, cv.WINDOW_NORMAL)
         cv.moveWindow(self.window_name, self.monitor.x - 1, self.monitor.y - 1)
         cv.setWindowProperty(self.window_name, cv.WND_PROP_FULLSCREEN,
                              cv.WINDOW_FULLSCREEN)
-        image = np.zeros(
-            (self.monitor.height, self.monitor.width), dtype='uint8')
-        cv.imshow(self.window_name, image)
-        cv.waitKey(0)
-        # cv.destroyAllWindows()
 
-    def updateArray(self, array, sleep=150):
+        while self.is_open:
+            image = self.queue.get()
+
+            cv.imshow(self.window_name, image)
+            cv.waitKey(self.display_interval)
+
+            self.queue.task_done()
+
+            if self.queue.empty():
+                self.queue.put(image)
+
+    def updateArray(self, array):
         """Update the SLM monitor with the supplied array.
-        Note that the array is not the same size as the SLM resolution,
-        the image will be deformed to fit the screen.
 
         Args:
             array (array_like): the array representing the mask that will be sent to the SLM.
-            sleep (Real, optional): Time in miliseconds that will be waited after calling this function.
-                This is important when one shows a series of masks in sequence, in which case one must wait for the SLM to properly dislplay each mask.
-                Defaults to 150.
+
         """
-        cv.imshow(self.window_name, array)
-        cv.waitKey(sleep)
+        self.queue.put(array)
 
     def close(self):
         """Closes the window associated with the SLMdisplay object.
         """
-        cv.destroyWindows(self.window_name)
+        self.is_open = False
+        cv.destroyWindow(self.window_name)
